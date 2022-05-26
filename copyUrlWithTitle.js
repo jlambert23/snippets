3d279650dc935b4b71fd877a617f2060bf070cbd
Jay Lambert <jb.lambert23@gmail.com>
2022-05-26 11:10:41 -0400

    Update copyUrlWithTitle.js

--- copyUrlWithTitle.js
+++ copyUrlWithTitle.js
@@ -1,3 +1,4 @@
+// Save this string as a bookmark:
 // javascript: (function () { function copyToClip(str) { function listener(e) { e.clipboardData.setData('text/html', str); e.clipboardData.setData('text/plain', str); e.preventDefault(); } document.addEventListener('copy', listener); document.execCommand('copy'); document.removeEventListener('copy', listener); } copyToClip('<a href="' + window.location.href + '">' + document.title + '</a>');})();
 
 (function () {
